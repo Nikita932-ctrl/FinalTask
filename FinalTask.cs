74935b0fba226929f45ef16cd2cda496b88b7a48
Nikita932-ctrl <65113631+Nikita932-ctrl@users.noreply.github.com>
2023-03-31 18:22:22 +0500

    Create FinalTask.cs

---
+++ FinalTask.cs
@@ -0,0 +1,95 @@
+﻿/*Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5. -> "1, 2, 3, 4, 5"
+M = 4; N = 8. -> "4, 6, 7, 8"
+
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15-> 120
+M = 4; N = 8. -> 30
+
+Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3->A(m, n) = 9
+m = 3, n = 2->A(m, n) = 2
+*/
+
+
+//Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+//M = 1; N = 5. -> "1, 2, 3, 4, 5"
+//M = 4; N = 8. -> "4, 6, 7, 8"
+
+/*
+void GetEvenNumber()
+{
+    
+    Console.WriteLine("Write Right Range number: ");
+    int rightRange = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Write Left Range number: ");
+
+    for (int leftRange = Convert.ToInt32(Console.ReadLine());leftRange <= rightRange; leftRange++)
+    {
+        
+        Console.WriteLine(leftRange);
+    }
+    
+}
+
+
+GetEvenNumber();
+*/
+
+//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+//M = 1; N = 15-> 120
+//M = 4; N = 8. -> 30
+
+/*
+int FoundNumbers(int leftRange,int rightRange)
+{
+    int count = 0;
+    for(int i = leftRange; i <= rightRange; i++)
+    {
+        count = count + i;
+    }
+    return count;
+}
+
+Console.WriteLine(FoundNumbers(4,8));
+
+*/
+
+/*Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3->A(m, n) = 9
+m = 3, n = 2->A(m, n) = 2
+*/
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+AkkermanFunction(m, n);
+
+
+// вызов функции Аккермана
+void AkkermanFunction(int m, int n)
+{
+    Console.Write(Akkerman(m, n));
+}
+
+// функция Аккермана
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
+}
